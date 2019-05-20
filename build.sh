051e1f5effbae1dcfe07752bcfcc495577638508
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-05-20 09:48:05 +0200

    Initial commit (move files from SharpFuzz repository)

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eux
+
+docker build -t libfuzzer-dotnet .
+docker container create --name extract-libfuzzer-dotnet libfuzzer-dotnet
+docker container cp extract-libfuzzer-dotnet:/app/libfuzzer-dotnet .
+docker container rm -f extract-libfuzzer-dotnet
+zip libfuzzer-dotnet.zip libfuzzer-dotnet
