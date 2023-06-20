92774a13eb71e4a9edabba3ab6122f5bb2c4e410
Nemanja Mijailovic <metalnem@mijailovic.net>
2023-06-20 10:56:18 +0200

    Remove outdated build scripts

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eux
-
-docker build -t libfuzzer-dotnet .
-docker container create --name extract-libfuzzer-dotnet libfuzzer-dotnet
-docker container cp extract-libfuzzer-dotnet:/app/libfuzzer-dotnet .
-docker container rm -f extract-libfuzzer-dotnet
-tar -czvf libfuzzer-dotnet.tar.gz libfuzzer-dotnet
